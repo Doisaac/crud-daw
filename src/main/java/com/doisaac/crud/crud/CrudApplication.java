71d1ea2544ad6476dbc41dd96a873004ee889a6f
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-04-13 10:30:30 -0600

    🐘 properties condition added

--- src/main/java/com/doisaac/crud/crud/CrudApplication.java
+++ src/main/java/com/doisaac/crud/crud/CrudApplication.java
@@ -1,10 +1,12 @@
 package com.doisaac.crud.crud;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudApplication {
+	@Value("${spring.datasource.url}")
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
